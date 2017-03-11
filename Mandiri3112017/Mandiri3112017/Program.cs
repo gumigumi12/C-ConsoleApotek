0c8198cb15da239f01c66bd6b29d690907c8e21a
Abdul Jalil <jalilabdul523@gmail.com>
2017-03-11 03:37:47 -0800

    Add files via upload

---
+++ Mandiri3112017/Mandiri3112017/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Q2M1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                {
+                    Console.WriteLine("----------------------Main Menu For---------------------");
+                    Console.WriteLine("========================================================");
+                    Console.WriteLine("+++              1)      Supplier                    +++");
+                    Console.WriteLine("+++              2)      Cashier                     +++");
+                    Console.WriteLine("+++              3)      Quit                        +++");
+                    Console.WriteLine("========================================================");
+                    Console.WriteLine("please enter the menu for :");
+                    int selected = Convert.ToInt32(Console.ReadLine());
+                    {
+                        switch (selected)
+                        {
+                            case 1:
+                                Console.WriteLine("========================================================");
+                                Console.WriteLine("+++                    Supplier                      +++");
+                                Console.WriteLine("========================================================");
+                                break;
+                            case 2:
+                                Console.WriteLine("========================================================");
+                                Console.WriteLine("+++                    Cashier                       +++");
+                                Console.WriteLine("========================================================");
+                                break;
+                            case 3:
+                                Console.WriteLine("========================================================");
+                                Console.WriteLine("----------------------Thanks You------------------------");
+                                Console.WriteLine("========================================================");
+                                return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
